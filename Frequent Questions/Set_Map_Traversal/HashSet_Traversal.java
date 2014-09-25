7e7c8290feaa7b02847e56190c886871a8c483d9
Wan Xin <xinwan@usc.edu>
2014-09-25 11:49:49 -0700

    add HashSet_Traversal

---
+++ Frequent Questions/Set_Map_Traversal/HashSet_Traversal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class HashSet_Traversal {
+	public static void main (String[] args) {
+		Set<String> set = new HashSet<String>();
+		set.add("a");
+		set.add("a");
+	    set.add("b");
+	    set.add("c");
+	    set.add("d");
+	    set.add("e");
+	    
+	    set.add("e");
+
+	    for (Iterator<String> iterator = set.iterator(); iterator.hasNext();) {
+	    	System.out.print(iterator.next() + " ");
+	    }
+
+	    System.out.println();
+	    System.out.println("*********************");
+
+	    for (String val : set) {
+	    	System.out.print(val + " ");
+	    }
+	}
+}
