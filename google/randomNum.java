39f71bd7fccfa80c4da23e8b52cf06dbbacd29d1
Wan Xin <xinwan@usc.edu>
2014-10-16 19:09:14 -0700

    randomNum.java

--- google/randomNum.java
+++ google/randomNum.java
@@ -1,13 +1,41 @@
 /*
 有 k 个数,排好序的,这些数都是 0 到 N 间的,写一个 randomnumber generate 来 generate 一个 0-N 间的数且不能是 k 里面的。
 */
-
+import java.util.*;
 public class randomNum {
-	public int random(int[] input) {
+	public static void main(String[] args) {
+		randomNum obj = new randomNum();
+		int[] input = {1, 2 ,3};
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+		System.out.println(obj.random(input, 5));
+
+
+	}
+
+	public int random(int[] input, int N) {
 		Random randGenerator = new Random();
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < input.length; i++) {
-			map.put(input[i], i + 1);
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int num : input) {
+			set.add(num);
 		}
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int idx = 0;
+		for (int i = 0; i < N; i++) {
+			if (!set.contains(i)) {
+				map.put(idx, i);
+				idx++;
+			} 
+
+
+		}
+
+		int randomInt = randGenerator.nextInt(N - input.length);
+		return map.get(randomInt);
 	}
 }
