ee9284ba3a480582c4cc34fd99540cd523fcc7a2
System Administrator <xinwan@usc.edu>
2014-12-18 14:14:56 -0800

    add code

--- google/IncSeq.java
+++ google/IncSeq.java
@@ -4,6 +4,7 @@ find the longest increasing sequence in an integer matrix in 4 directions (up do
 8765,  output should be 12345678
 这个题我用dfs＋backtracking做的
 */
+import java.util.*;
 public class IncSeq {
 	public ArrayList<Integer> dfs(int[][] matrix) {
 		int m = matrix.length;
@@ -12,11 +13,13 @@ public class IncSeq {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
-				boolean[][] flag = boolean[m][n];
+				boolean[][] flag =  new boolean[m][n];
 				ArrayList<Integer> tmp = new ArrayList<Integer>();
 				tmp.add(matrix[i][j]);
-				boolean[i][j] = true;
+				flag[i][j] = true;
 				dfsHelper(result, tmp, flag, i, j, matrix);
+				flag[i][j] = false;
+				tmp.remove(tmp.size() - 1);
 			}
 		}
 
@@ -25,6 +28,7 @@ public class IncSeq {
 
 	private void dfsHelper(ArrayList<Integer> result, ArrayList<Integer> tmp, boolean[][] flag, int i, int j, int[][] matrix) {
 		int[][] dirs = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+		boolean flag1 = true;
 		for (int[] dir : dirs) {
 			if (isValid(flag, i, j, dir, matrix)) {
 				flag[i + dir[0]][j + dir[1]] = true;
@@ -32,13 +36,15 @@ public class IncSeq {
 				dfsHelper(result, tmp, flag, i + dir[0], j + dir[1], matrix);
 				tmp.remove(tmp.size() - 1);
 				flag[i + dir[0]][j + dir[1]] = false;
+				flag1 = false;
 			}
 		}
-
-		if (result.size() < tmp.size()) {
-			result.clear();
-			result.addAll(tmp);
-			return;
+		if (flag1) {
+			if (result.size() < tmp.size()) {
+				result.clear();
+				result.addAll(tmp);
+				return;
+			}
 		}
 	}
 
@@ -60,5 +66,13 @@ public class IncSeq {
 
 
 	}
+
+	public static void main(String[] args) {
+		int[][] input = {{1, 2, 3, 4}, {8, 7, 6, 5}};
+		IncSeq obj = new IncSeq();
+		System.out.println(obj.dfs(input));
+	}
+
+	
 
 }
